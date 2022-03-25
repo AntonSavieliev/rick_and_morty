f68dae6b766621002cb520170787932a2f565d49
Anton Savieliev <ant.savelev@gmail.com>
2022-03-25 14:01:50 +0200

    adding DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/common/app_colors.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_bloc.dart';
+import 'package:rick_and_morty/locator_service.dart' as di;
 
-void main() {
-  runApp(MyApp());
+import 'feature/presentation/pages/person_screen.dart';
+import 'locator_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PersonListCubit>(
+            create: (context) => sl<PersonListCubit>()..loadPerson()),
+        BlocProvider<PersonSearchBloc>(
+            create: (context) => sl<PersonSearchBloc>()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          backgroundColor: AppColors.mainBackground,
+          scaffoldBackgroundColor: AppColors.mainBackground,
+        ),
+        home: const HomePage(),
       ),
-      home: HomePage(),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
 
